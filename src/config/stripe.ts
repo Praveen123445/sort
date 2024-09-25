d73c31d421583afa778b3cf31263cff9288b68a2
Praveen123445 <Pravin324@gmail.com>
2024-09-25 21:28:31 +0530

    first comit

---
+++ src/config/stripe.ts
@@ -0,0 +1,29 @@
+export const PLANS = [
+    {
+        name: "Free",
+        slug: "free",
+        quota: 10,
+        pagesPerPdf: 5,
+        price: {
+            amount: 0,
+            priceIds: {
+                test: '',
+                production: ''
+            },
+        },
+    },
+
+    {
+        name: "Pro",
+        slug: "pro",
+        quota: 50,
+        pagesPerPdf: 25,
+        price: {
+            amount: 14,
+            priceIds: {
+                test: 'pk_test_51Q2qJlSJBtuubriwMpUq5iRvnrlLbYl2vAfXPkNv11MInqFMEdLz0oMSe4jA4jZBx0N11F815ZokZPUEAyl9sC1D00c4Hs4K6K',
+                production: ''
+            },
+        },
+    },
+]
